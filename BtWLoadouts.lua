38d2e084354a6eac5081a509933931390afd04a7
Breeni <breeni@mythision.com>
2019-09-24 14:33:05 +0100

    Added LDB icon

--- BtWLoadouts.lua
+++ BtWLoadouts.lua
@@ -8610,3 +8610,31 @@ eventHandler:SetScript("OnUpdate", function (self)
 		ContinueActivateProfile();
     end
 end)
+
+do
+	-- LDB launcher
+	local LDB = LibStub and LibStub("LibDataBroker-1.1", true)
+	if LDB then
+		LDB:NewDataObject("BtWLoadOuts", {
+			type = "launcher",
+            label = L["BtWLoadouts"],
+			icon = [[Interface\ICONS\Ability_marksmanship]],
+			OnClick = function(clickedframe, button)
+				if button == "LeftButton" then
+					BtWLoadoutsFrame:SetShown(not BtWLoadoutsFrame:IsShown());
+				elseif button == "RightButton" then
+					if not BtWLoadoutsMinimapButton.Menu then
+						BtWLoadoutsMinimapButton.Menu = CreateFrame("Frame", BtWLoadoutsMinimapButton:GetName().."Menu", BtWLoadoutsMinimapButton, "UIDropDownMenuTemplate");
+						UIDropDownMenu_Initialize(BtWLoadoutsMinimapButton.Menu, BtWLoadoutsMinimapMenu_Init, "MENU");
+					end
+					
+					ToggleDropDownMenu(1, nil, BtWLoadoutsMinimapButton.Menu, clickedframe, 0, -5);
+				end
+			end,
+			OnTooltipShow = function(tooltip)
+				tooltip:SetText(L["BtWLoadouts"], 1, 1, 1);
+				tooltip:AddLine(L["Click to open BtWLoadouts.\nRight Click to enable and disable settings."], nil, nil, nil, true);
+			end,
+		})
+	end
+end
