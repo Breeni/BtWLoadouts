7d7f1837ca178620cc100c7e465392d0e22a7de6
Breeni <breeni@mythision.com>
2019-12-02 22:29:11 +0000

    Split translations into their own files

---
+++ Localizations.lua
@@ -0,0 +1,29 @@
+-- Define Localization table
+
+local _, Internal = ...
+
+local L = {}
+setmetatable(L, {
+    __index = function (self, key)
+        return key
+    end,
+})
+Internal.L = L
+
+-- Fallbacks for items missing translations
+L["Talents"] = TALENTS
+L["PvP Talents"] = PVP_TALENTS
+L["Equipment"] = BAG_FILTER_EQUIPMENT
+L["New Set"] = PAPERDOLL_NEWEQUIPMENTSET
+L["Activate"] = TALENT_SPEC_ACTIVATE
+L["Delete"] = DELETE
+L["Name"] = NAME
+L["Specialization"] = SPECIALIZATION
+L["None"] = NONE
+L["New"] = NEW
+L["World"] = WORLD
+L["Dungeons"] = DUNGEONS
+L["Raids"] = RAIDS
+L["Arena"] = ARENA
+L["Battlegrounds"] = BATTLEGROUNDS
+L["Other"] = OTHER
---
+++ Localizations.ruRU.lua
@@ -0,0 +1,37 @@
+-- Russian translations. Thanks to Void_OW on Curse
+
+if GetLocale() ~= "ruRU" then return; end
+
+local _, Internal = ...
+local L = Internal.L
+
+L["A tome is needed to continue equiping your set."] = "Для применения этого набора необходим фолиант."
+L["Activate profile %s?"] = "Применить профиль %s?"
+L["Activate spec %s?\nThis set will require a tome or rested to activate"] = "Применить специализацию %s? Этот набор потребует фолианта или зоны отдыха для применения"
+L["Activate spec %s?\nThis will use a Tome"] = "Применить специализацию %s? Это задействует Фолиант"
+L["Activate the following profile?\n"] = "Применить этот профиль?"
+L["Activate your profile."] = "Задействуйте ваш профиль."
+L["Any"] = "Любой"
+L["Are you sure you wish to delete the set \"%s\", this set is in use by one or more profiles. This cannot be reversed."] = "Вы уверены что хотите удалить набор \"%s\"? Этот набор используется в одном или нескольких профилях. Это действие невозможно отменить."
+L["Are you sure you wish to delete the set \"%s\". This cannot be reversed."] = "Вы уверены что хотите удалить набор \"%s\"? Это действие нельзя отменить."
+L["Battle For Azeroth"] = "Battle For Azeroth"
+L["Can not edit equipment manager sets."] = "Изменять наборы экипировки нельзя."
+L["Can not equip sets for other characters."] = "Применить наборы для других персонажей нельзя."
+L["Cannot create any more macros"] = "Невозможно создать больше макросов."
+L["Cannot create macros while in combat"] = "Невозможно создать макрос в бою"
+L["Change the name of your new profile."] = "Измените имя вашего нового профиля."
+L["Click to open BtWLoadouts.\nRight Click to enable and disable settings."] = "Нажмите для запуска BtWLoadouts. Правая кнопка мыши открывает настройки."
+L["Conditions"] = "Условия"
+L["Could not find a valid set"] = "Нет подходящего набора"
+L["Create a talent set for your new profile."] = "Создайте набор талантов для вашего нового профиля."
+L["Essences"] = "Сущности"
+L["New %s Equipment Set"] = "Новый набор экипировки %s"
+L["New %s Set"] = "Новый набор %s"
+L["New Condition Set"] = "Новый набор условий"
+L["New Profile"] = "Новый профиль"
+L["Other"] = "Другое"
+L["Profile"] = "Профиль"
+L["Profiles"] = "Профили"
+L["Shift+Left Mouse Button to ignore a slot."] = "Shift+Левая кнопка мыши игнорирует слот."
+L["Show minimap icon"] = "Показывать кнопку на мини-карте"
+L["To begin, create a new set."] = "Для начала, создайте новый набор."
