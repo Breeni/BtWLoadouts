45efa257176ebf23059eec8d32c51619d95354af
Breeni <breeni@mythision.com>
2020-08-29 22:19:41 +0100

    Fixed checking if neck quest is done in Shadowlands

--- Essences.lua
+++ Essences.lua
@@ -36,6 +36,7 @@ local function GetEssenceSet(id)
 		return BtWLoadoutsSets.essences[id];
 	end
 end
+local IsQuestFlaggedCompleted = IsQuestFlaggedCompleted and C_QuestLog.IsQuestFlaggedCompleted or IsQuestFlaggedCompleted
 local function CanActivateEssences()
 	return IsQuestFlaggedCompleted(55618) -- The Heart Forge quest
 end
