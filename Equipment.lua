c8964fa1f07cb758480c3a0145225aa1564bbcc9
Breeni <breeni@mythision.com>
2020-12-12 15:07:23 +0000

    Fixed a error where equipment set information isnt available during first player login

--- Equipment.lua
+++ Equipment.lua
@@ -975,16 +975,18 @@ local function CombineEquipmentSets(result, state, ...)
 			if set.managerID then -- Just making sure everything is up to date
 				local ignored = C_EquipmentSet.GetIgnoredSlots(set.managerID);
 				local locations = C_EquipmentSet.GetItemLocations(set.managerID);
-				for inventorySlotId=INVSLOT_FIRST_EQUIPPED,INVSLOT_LAST_EQUIPPED do
-					set.ignored[inventorySlotId] = ignored[inventorySlotId] and true or nil;
+				if ignored and locations then
+					for inventorySlotId=INVSLOT_FIRST_EQUIPPED,INVSLOT_LAST_EQUIPPED do
+						set.ignored[inventorySlotId] = ignored[inventorySlotId] and true or nil;
 
-					local location = locations[inventorySlotId] or 0;
-					if location > -1 then -- If location is -1 we ignore it as we cant get the item link for the item
-						set.equipment[inventorySlotId] = GetItemLinkByLocation(location);
-					end
-					set.locations[inventorySlotId] = location;
-					if set.extras[inventorySlotId] then
-						wipe(set.extras[inventorySlotId])
+						local location = locations[inventorySlotId] or 0;
+						if location > -1 then -- If location is -1 we ignore it as we cant get the item link for the item
+							set.equipment[inventorySlotId] = GetItemLinkByLocation(location);
+						end
+						set.locations[inventorySlotId] = location;
+						if set.extras[inventorySlotId] then
+							wipe(set.extras[inventorySlotId])
+						end
 					end
 				end
 			else
